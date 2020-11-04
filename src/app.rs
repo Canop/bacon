be9537cd6d12853ee4dc5a6b81161d9da3161f52
Nikhil Mitra <nikhil@secondspectrum.com>
2020-11-04 12:32:25 +0100

    Initial support for workspaces

--- src/app.rs
+++ src/app.rs
@@ -15,17 +15,120 @@ use {
         fs,
         io::Write,
         path::PathBuf,
+        process::Command
     },
+    serde::{Serialize, Deserialize},
     termimad::{Event, EventSource},
 };
 
+// Represents a subset of JSON returned by `cargo metadata`
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CargoMetadata {
+    #[serde(rename = "packages")]
+    packages: Vec<Package>,
+
+    #[serde(rename = "workspace_members")]
+    workspace_members: Vec<String>,
+
+    #[serde(rename = "target_directory")]
+    target_directory: String,
+
+    #[serde(rename = "version")]
+    version: i64,
+
+    #[serde(rename = "workspace_root")]
+    workspace_root: String,
+
+    #[serde(rename = "metadata")]
+    metadata: Option<serde_json::Value>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Package {
+    #[serde(rename = "name")]
+    name: String,
+
+    #[serde(rename = "version")]
+    version: String,
+
+    #[serde(rename = "id")]
+    id: String,
+
+    #[serde(rename = "license")]
+    license: Option<String>,
+
+    #[serde(rename = "license_file")]
+    license_file: Option<String>,
+
+    #[serde(rename = "description")]
+    description: Option<String>,
+
+    #[serde(rename = "targets")]
+    targets: Vec<Target>,
+
+    #[serde(rename = "source")]
+    source: Option<String>,
+
+    #[serde(rename = "manifest_path")]
+    manifest_path: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Target {
+    #[serde(rename = "name")]
+    name: String,
+
+    #[serde(rename = "src_path")]
+    src_path: String,
+
+    #[serde(rename = "edition")]
+    edition: String,
+
+    #[serde(rename = "doctest")]
+    doctest: bool,
+
+    #[serde(rename = "required-features")]
+    required_features: Option<Vec<String>>,
+}
+
+fn find_folders_to_watch(root_dir:&PathBuf)->Result<Vec<PathBuf>>{
+
+    let output = Command::new("cargo").current_dir(root_dir).arg("metadata").arg("--format-version").arg("1").output()?;
+
+    let output_string = String::from_utf8(output.stdout)?;
+    let metadata:CargoMetadata = serde_json::from_str(&output_string)?;
+
+    let mut folders_to_watch = vec![];
+    for item in metadata.packages{
+        if item.source.is_none(){
+            // We're only concerned with items where the source is None
+            for target in item.targets{
+                if target.src_path.ends_with("lib.rs") || target.src_path.ends_with("main.rs"){
+                    // targets can contain the build script as well, so we need to eliminate them.
+
+                    let target_folder = PathBuf::from_str(&target.src_path)?;
+                    let target_folder_parent = PathBuf::from(target_folder.parent().expect("parent of target is a root folder."));
+                    if target_folder_parent.ends_with("src"){
+                        // to ensure misc binaries are not counted, such as
+                        // src/binaries/foo_main.rs
+                        folders_to_watch.push(target_folder_parent)
+                    }
+                }
+            }
+        }
+    }
+    Ok(folders_to_watch)
+}
+
 pub fn run(w: &mut W, args: Args) -> Result<()> {
+
     let root_dir = args.root.unwrap_or_else(||env::current_dir().unwrap());
     let root_dir: PathBuf = fs::canonicalize(&root_dir)?;
+    let src_dirs = find_folders_to_watch(&root_dir).unwrap();
     debug!("root_dir: {:?}", &root_dir);
-    let src_dir = root_dir.join("src");
     let cargo_toml_file = root_dir.join("Cargo.toml");
-    if !src_dir.exists() || !cargo_toml_file.exists() {
+    if !cargo_toml_file.exists() {
         return Err(anyhow!(
             "bacon must be launched either\n\
             * in a rust project directory\n\
@@ -56,7 +159,10 @@ pub fn run(w: &mut W, args: Args) -> Result<()> {
         }
         Err(e) => warn!("watch error: {:?}", e),
     })?;
-    watcher.watch(src_dir, RecursiveMode::Recursive)?;
+
+    for src_dir in src_dirs.iter(){
+        watcher.watch(src_dir, RecursiveMode::Recursive)?;
+    }
     watcher.watch(cargo_toml_file, RecursiveMode::NonRecursive)?;
 
     let computer = Computer::new(root_dir, args.clippy)?;
