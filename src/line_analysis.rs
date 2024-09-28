6aca1cf369be1750694956b2636f8e59b7f3cdae
Denys Séguret <cano.petrole@gmail.com>
2024-09-28 11:36:23 +0200

    recognize panic location in test (#211)
    Fix #208

--- src/line_analysis.rs
+++ src/line_analysis.rs
@@ -44,6 +44,9 @@ impl From<&CommandOutputLine> for LineAnalysis {
             } else if regex_is_match!(r#"^\s+--> [^:\s'"]+:\d+:\d+$"#, content) {
                 // this comes up in test failures to compile
                 LineType::Location
+            } else if regex_is_match!(r#"^thread '.+' panicked at [^:\s'"]+:\d+:\d+:$"#, content) {
+                // this comes up in test failures
+                LineType::Location
             } else {
                 LineType::Normal
             }
