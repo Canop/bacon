b627f37c006d47364a5a86e7dced8105c8a4a823
Bigo <1781140+crisidev@users.noreply.github.com>
2025-01-10 13:24:44 +0000

    Allow bacon to be consumed as a nix flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk { };
+        bacon = naersk'.buildPackage {
+          src = ./.;
+        };
+      in
+      {
+        # For `nix build` & `nix run`:
+        defaultPackage = bacon;
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+          ];
+        };
+
+        # Overlay for package usage in other Nix configurations
+        overlay = final: prev: {
+          bacon = bacon;
+        };
+      }
+    );
+}
